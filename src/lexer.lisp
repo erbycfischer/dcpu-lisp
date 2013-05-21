8d12fb3869cdb942b71d407fb87087bd6cbf9288
Erby Fischer <erbycfischer@mac.com>
2013-05-21 16:32:09 +0200

    Commented lexer.lisp

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -1,13 +1,18 @@
+;; *code* will contain the lisp program being operated one
 (defparameter *code* nil)
 
+;; this will convert the lisp program into lexemes
 (defun lex () 
 	(format t "Made it to lex! ~%"))
 
+;; reads a file given a filename
 (defun read-file (file-name)
-	;; credits due
+	;; credits due -- takes a file as a stream
+	;; and reads the whole file at onece
 	(flet	((slurp-stream (stream) 
 					(let ((seq (make-string (file-length stream)))) 
 						(read-sequence seq stream) seq)))
+		;; checks if the file exists
 		(if (probe-file file-name)
 				(slurp-stream (open file-name))
 				(progn
@@ -16,8 +21,10 @@
 					'nil))))
 
 (defun main () 
+	;; if we have less than 2 arguments then they definiely did not provide a filename
 	(if (< (length *posix-argv*) 2)
 		(format t "You need to provide a lisp file to compile ~%")
+		;; sets *code* to the whole lisp program
 		(setf *code* (read-file (cadr *posix-argv*))))
 	(if (equal *code* 'nil)
 		(return-from main)
