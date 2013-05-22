6f910438cfaf077ee4b25b04a4448c1618a5e621
Braden Groom <braden.groom@gmail.com>
2013-05-21 23:17:27 -0500

    first implementation of lexer

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -1,9 +1,34 @@
+(ql:quickload :cl-ppcre)
+
 ;; *code* will contain the lisp program being operated one
-(defparameter *code* nil)
+(defparameter *code* 'nil)
+;; *last-match* will contain the type of the last match
+(defparameter *last-match* 'nil)
 
 ;; this will convert the lisp program into lexemes
 (defun lex () 
-	(format t "Made it to lex! ~%"))
+	(loop for token = (match *code*) until (equal token 'nil) do (progn (print token) (remove-match *last-match*))))
+
+(defun match (string)
+	(progn (let ((match (cl-ppcre:scan-to-strings "^\\(" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'LPAR match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^\\)" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'RPAR match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^[a-zA-Z][\\w\\+\\-\\*]+" string)))
+				(when (and (> (length match) 0) (equal *last-match* "(")) (progn (setf *last-match* match) (return-from match (cons 'FUNC match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^[a-zA-Z][\\w\\+\\-\\*]+" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'VAR match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^[-+]?[0-9]*\\.?[0-9]+" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'NUM match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^\\\"(\\\\.|.)*\\\"" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'STR match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^\\\'(\\w|\\-)+" string)))
+				(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'SYM match)))))
+			(let ((match (cl-ppcre:scan-to-strings "^\\s+" string)))
+				(when (> (length match) 0) (return-from match (match (remove-match match)))))))
+
+(defun remove-match (match)
+	(setf *code* (subseq *code* (length match))))
 
 ;; reads a file given a filename
 (defun read-file (file-name)
