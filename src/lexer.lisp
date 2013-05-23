f64f4fa5e36d5eaf2ade5f8572cb95c0642b8e8d
Braden Groom <braden.groom@gmail.com>
2013-05-23 01:23:36 -0500

    rewrote (match string) and (lex).  also debugged them a bit.

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -5,27 +5,26 @@
 ;; *last-match* will contain the type of the last match
 (defparameter *last-match* 'nil)
 
+(defparameter *tokens* '(("^\\(" 'LPAR)
+					("^\\)" 'RPAR)
+					("^[\\w\\+\\-\\*:<>]+" 'FUNC)
+					("^[a-zA-Z][\\w\\+\\-\\*:]+" 'VAR)
+					("^[-+]?[0-9]*\\.?[0-9]+" 'NUM)
+					("^\\\"(\\\\.|.)*\\\"" 'STR)
+					("^\\\'\\w+" 'SYM)))
+
+(defun match (string) (flet ((check-match (token-pair string)
+								(let ((match-to-check (cl-ppcre:scan-to-strings (car token-pair) string)))
+									(when (> (length match-to-check) 0) (progn (setf *last-match* match-to-check) (return-from match (cons (cadr token-pair) match-to-check)))))
+								(let ((match-to-check (cl-ppcre:scan-to-strings "^\\s+" string)))
+									(when (> (length match-to-check) 0) (return-from match (match (remove-match match-to-check)))))))
+				(loop for token in *tokens* do (let ((match-found (check-match token string)))
+					(when (not (equal match-found 'nil)) (progn (remove-match *last-match*) (return-from match match-found)))))))
+
 ;; this will convert the lisp program into lexemes
 (defun lex () 
 	(loop for token = (match *code*) until (equal token 'nil) do (progn (print token) (remove-match *last-match*))))
 
-(defun match (string)
-	(let ((match (cl-ppcre:scan-to-strings "^\\(" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'LPAR match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^\\)" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'RPAR match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^[a-zA-Z][\\w\\+\\-\\*]+" string)))
-		(when (and (> (length match) 0) (equal *last-match* "(")) (progn (setf *last-match* match) (return-from match (cons 'FUNC match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^[a-zA-Z][\\w\\+\\-\\*]+" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'VAR match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^[-+]?[0-9]*\\.?[0-9]+" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'NUM match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^\\\"(\\\\.|.)*\\\"" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'STR match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^\\\'(\\w|\\-)+" string)))
-		(when (> (length match) 0) (progn (setf *last-match* match) (return-from match (cons 'SYM match)))))
-	(let ((match (cl-ppcre:scan-to-strings "^\\s+" string)))
-		(when (> (length match) 0) (return-from match (match (remove-match match))))))
 
 (defun remove-match (match)
 	(setf *code* (subseq *code* (length match))))
